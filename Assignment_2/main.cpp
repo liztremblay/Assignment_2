c4cef83cf1333ed0642b44bac27dce07128e1189
Elizabeth Tremblay <liztremblay01@gmail.com>
2025-02-19 12:26:41 -0500

    Initial Commit

---
+++ Assignment_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment_2
+//
+//  Created by Elizabeth Tremblay on 2025-02-19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
